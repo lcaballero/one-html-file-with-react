eb6a73902c85e7ab1ea22f513f074b87b89d7b69
lucas <lucas@section.io>
2022-03-13 23:36:55 -0600

    Using downloaded version of react and babel to lock the versions and work off-line.

--- run.sh
+++ run.sh
@@ -1,17 +1,41 @@
-#!/bin/build
+#!/bin/bash
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 
-bash() {
+build() {
   mkdir -p .dist
   (
-    cd "$DIR" &&
-      cp index.html main.js "$DIR/.dist"
+    cd "$DIR/src" &&
+      cp index.html \
+         main.js \
+         react-dom.development.js react.development.js babel.min.js \
+         main.css reset.css has-layout.css \
+         "$DIR/.dist"
   )
 }
 
 clean() {
   rm -rf .dist
+}
+
+start::server() {
+  ulimit -n 8192 && \
+    caddy -root .dist \
+          -pidfile pid \
+          -port 1111 2>&1 > /dev/null &
+}
+
+stop::server() {
+  if [ -f "$DIR/pid" ]; then
+    kill -9 "$(< "$DIR/pid")"
+    rm "$DIR/pid"
+  fi
+}
+
+show() {
+  cat <<EOF
+DIR = $DIR
+EOF
 }
 
 "$@"
---
+++ src/main.js
@@ -0,0 +1,29 @@
+const { useState } = React
+
+const Item = ({name, changeName}) => {
+  return <li onClick={()=>changeName(name)}>{name}</li>
+}
+
+const names = ["world", "batman", "superman", "wonder woman"]
+const Greeting = () => {
+  const [name, changeName] = useState(() => {
+    console.log("use state")
+    return "world"
+  })
+  const updateName = (event) => {
+    console.log('event:', event)
+    //changeName(newName)
+  }
+  return <div>
+  <ul className="choices">
+  {
+    names.map(name => <Item {...{name, changeName}} />)
+  }
+  </ul>
+  <p>Hello, {name}!</p>
+  </div>
+}
+ReactDOM.render(
+  <Greeting />,
+  document.getElementById('root')
+);
